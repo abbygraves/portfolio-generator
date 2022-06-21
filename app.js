d9f2956385e96a09b90ff217a2ce734c2e7fa9ff
abbygraves <abbygraves14@gmail.com>
2022-06-21 04:03:14 -0500

    add style sheet

--- app.js
+++ app.js
@@ -184,16 +184,21 @@ const promptProject = portfolioData => {
 
 
 
-// promptUser()
-//   .then(promptProject)
-//   .then(portfolioData => {
-const pageHTML = generatePage(mockData);
-
-
-
-// ⬇︎ CREATE AND WRITE THE THE HTML DOC
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw new Error(err);
-
-  console.log('Page created! Check out index.html in this directory to see it!');
-});
+promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
